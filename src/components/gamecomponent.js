947077510973dff58fd8e82c2fe15f600bf94bfb
Esteban <esteban29712@gmail.com>
2022-03-06 18:42:55 -0500

    first

---
+++ src/components/gamecomponent.js
@@ -0,0 +1,24 @@
+import React from 'react'; 
+import '../App.css';
+
+
+function Home () {
+
+    return (
+        <div id="container" className="center" >  
+            <div id='gameBoard'>
+                <div id="numberbox one">sdfsa</div>
+                <div id="numberbox one"></div>
+                <div id="numberbox one"></div>
+                <div id="numberbox one"></div>
+                <div id="numberbox one"></div>
+                <div id="numberbox one"></div>
+                <div id="numberbox one">dsf</div>
+                <div id="numberbox one"></div>
+                <div id="numberbox one"></div>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
